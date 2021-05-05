d850b14d10dee2811a0d23114ce83925ce7efe20
lm <lmweb@centrum.cz>
2021-05-05 11:51:43 +0200

    založení projektu v eclipse

---
+++ git1/src/git1/Main.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package git1;
+
+import java.awt.desktop.SystemEventListener;
+
+/**
+ * @author masic
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("git");
+	}
+
+}
